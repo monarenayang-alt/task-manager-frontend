fac2c57908bc742bc3f02e62762e94fd959ebcae
winonahmonare <mw24019724@biust.ac.bw>
2026-04-30 21:32:03 +0200

    Fix Home page buttons rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div className="text-center">
+    <div style={{ textAlign: "center" }}>
       <h1>Welcome to Task Manager</h1>
-      <p className="lead">
+
+      <p>
         This application helps you add, manage, and track your daily tasks.
       </p>
+
+      <div style={{ marginTop: "20px" }}>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+        <Link to="/list" style={{ marginLeft: "10px" }}>
+          <button>View Tasks</button>
+        </Link>
+
+        <Link to="/add" style={{ marginLeft: "10px" }}>
+          <button>Add Task</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
